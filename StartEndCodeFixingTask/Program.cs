bfa84e45379d47823953b0ae9f3809bfed95e1ec
Neil Dhiman <21NDhiman@heckgrammar.co.uk>
2025-03-16 13:28:12 +0000

    ssdfsf

--- StartEndCodeFixingTask/Program.cs
+++ StartEndCodeFixingTask/Program.cs
@@ -9,6 +9,16 @@
             // Show the test outputs for the test table in the Readme
             // Put your evidence where it says Do the coding task in C#
 
+            Console.WriteLine("Enter start year: ");
+            int startyear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter end year: ");
+            int endyear = Convert.ToInt32(Console.ReadLine());
+
+            if (startyear < endyear && 2000 > startyear) 
+            { 
+                int difference = endyear - startyear;
+                Console.WriteLine("The differenc between the years are: " + difference);
+            }
 
         }
     }
